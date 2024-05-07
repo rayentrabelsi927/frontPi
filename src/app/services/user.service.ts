c09273464f10f90425554c95d9836ee206229d92
FeresMrad <mohamedferes.mrad@esprit.tn>
2024-05-07 02:07:04 +0100

    update user2

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -20,4 +20,8 @@ export class UserService {
     const url = `${this.baseURL}/update/${userId}`;
     return this.httpClient.put<User>(url, user);
   }
+
+//   updateUser(user: User): Observable<User> {
+//     return this.httpClient.put<User>(`${this.baseURL}/update`, user);
+// }
 }
